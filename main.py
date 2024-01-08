7c565d5b34e76fe4d59fb2207f9ff86b53b10657
Tyujhty <fannybaquey@gmail.com>
2024-01-08 17:11:40 +0100

    feat: Correct the function that checks the ressources

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from data import MENU, resources
 
-
 def check_choice(consumer_choice):
     coffee_data = {}
     for coffee in MENU:
@@ -9,12 +8,17 @@ def check_choice(consumer_choice):
             return coffee_data
 
 def check_resources(coffee_selection):
-    for ingredient in coffee_selection['ingredients']:
-        if coffee_selection['ingredients'][ingredient] > resources[ingredient]:
-            print(f"Sorry there is not enough {ingredient}.")
-        else:
-            resources[ingredient] -= coffee_selection['ingredients'][ingredient]
-            print(resources[ingredient])
+    coffer_ingredients = coffee_selection['ingredients']
+    missing_ingredient = []
+    for ingredient in coffer_ingredients:
+        if coffer_ingredients[ingredient] > resources[ingredient]:
+            missing_ingredient.append(ingredient)
+    if missing_ingredient:
+        print(f"Sorry there is not enough {', '.join(missing_ingredient)}.")
+    else:
+        for ingredient in coffer_ingredients:
+            resources[ingredient] -= coffer_ingredients[ingredient]
+            return resources
 
 consumer_choice = input("What would you like?: (espresso/latte/cappuccino): ")
 coffee_selection = check_choice(consumer_choice)
